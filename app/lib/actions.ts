d6fb349ef1728e51ed42b4628400b56c37f8186a
chanonejc <chanonejc@gmail.com>
2025-08-27 05:25:21 +0700

    editing invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,9 @@
 'use server';
 
 import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
 import postgres from 'postgres';
+import { redirect } from 'next/navigation';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -21,7 +23,15 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-  const amountInCents = amount * 100; 
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0]; 
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
   // Test it out:
-  console.log({ customerId, amountInCents, status });
+  //console.log({ customerId, amountInCents, status });
 }
